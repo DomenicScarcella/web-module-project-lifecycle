3593bb39fe6c462082199c827177232835973c1f
Domenic Scarcella <ls41513@yahoo.com>
2021-03-09 21:44:20 -0500

    MVP?

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom';
 const Card = ({user}) => {
     console.log(user);
     return (
-        <div className=''>
+        <div>
             <div>
                 <img width='100px' src={user.avatar_url} alt='user avatar'/>
             </div>
             <div>
                 <h2>{user.name}</h2>
                 <h3><i>{user.login}</i></h3>
-                <p>{user.location}</p>
-                <p>{user.html_url}</p>
-                <p>{user.followers}</p>
-                <p>{user.following}</p>
-                <p>{user.bio}</p>
+                <p>Location: {user.location}</p>
+                <p>Profile link: {user.html_url}</p>
+                <p>Followers: {user.followers}</p>
+                <p>Following: {user.following}</p>
+                <p>Bio: {user.bio}</p>
             </div>
         </div>
     );
--- my-app/src/components/CardList.js
+++ my-app/src/components/CardList.js
@@ -7,22 +7,22 @@ const CardList = ({followers}) => {
 
 
     return (
-        <div className=''>
+        <div>
             {
                 followers.map(follower => {
                     return(
-                        <div className=''>
+                        <div>
                             <div>
                                 <img width='100px' src={follower.avatar_url} alt='user avatar'/>
                             </div>
                             <div>
-                                <h2>{follower.name}</h2>
+                                {/* <h2>{follower.name}</h2> */}
                                 <h3><i>{follower.login}</i></h3>
-                                <p>{follower.location}</p>
+                                {/* <p>{follower.location}</p>
                                 <p>{follower.html_url}</p>
                                 <p>{follower.followers}</p>
                                 <p>{follower.following}</p>
-                                <p>{follower.bio}</p>
+                                <p>{follower.bio}</p> */}
                             </div>
                         </div>
                     )
